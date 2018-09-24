3f84d644ac701095a7f0fe3d339d2840169c8aff
udayaditya singh <udayaditya.singh@gmail.com>
2018-09-24 16:19:20 +0530

    bufferToggle

--- src/app/rxjs-buffer-toggle/rxjs-buffer-toggle.component.ts
+++ src/app/rxjs-buffer-toggle/rxjs-buffer-toggle.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { interval, fromEvent, empty } from 'rxjs';
+import { bufferToggle } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs-buffer-toggle',
@@ -12,15 +13,37 @@ export class RxjsBufferToggleComponent implements OnInit {
 
   ngOnInit() {
     //emit value every second
-    const myInterval = interval(1000);
+    const sourceInterval = interval(1000);
     
     //start first buffer after 5s, and every 5s after
     const startInterval = interval(5000);
 
     const closingInterval = val => {
-      console.log(`Value ${val} emitted, starting buffer! Closing in 3s!`)
+      // console.log(`Value ${val} emitted, starting buffer! Closing in 3s!`)
         return interval(3000)
     }
+
+    //every 5s a new buffer will start, collecting emitted values for 3s then emitting buffered values
+    const bufferToggleInterval = sourceInterval.pipe(bufferToggle(startInterval, closingInterval));
+
+    //log to console
+    //ex. emitted buffers [4,5,6]...[9,10,11]
+    const subscribe = bufferToggleInterval.subscribe((val) => {
+      // console.log('Emitted Buffer : ', val);
+      
+    })
+
+    this.onclick()
+  }
+
+  onclick(){
+    // Every other second, emit the click events from the next 500ms
+    var clicks = fromEvent(document, 'click');
+    var openings = interval(1000);
+    var buffered = clicks.pipe(bufferToggle(openings, i =>
+      i % 2 ? interval(500) : empty()
+    ));
+    buffered.subscribe(x => console.log(x));
   }
 
 }
