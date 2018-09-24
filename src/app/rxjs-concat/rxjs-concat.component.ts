993a159fe93defe8f5d7be2096255afda4d3557e
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-09-24 23:37:17 +0530

    concataLL

--- src/app/rxjs-concat/rxjs-concat.component.ts
+++ src/app/rxjs-concat/rxjs-concat.component.ts
@@ -12,32 +12,32 @@ export class RxjsConcatComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    //emits 1,2,3
-    const sourceOne = of(1, 2, 3);
-    //emits 4,5,6
-    const sourceTwo = of(4, 5, 6);
-    //emit values from sourceOne, when complete, subscribe to sourceTwo
-    const concatSource = sourceOne.pipe(concat(sourceTwo));
-    //output: 1,2,3,4,5,6
-    const subscribe = concatSource.subscribe(val => console.log('Example 1: Basic concat:', val));
+    // //emits 1,2,3
+    // const sourceOne = of(1, 2, 3);
+    // //emits 4,5,6
+    // const sourceTwo = of(4, 5, 6);
+    // //emit values from sourceOne, when complete, subscribe to sourceTwo
+    // const concatSource = sourceOne.pipe(concat(sourceTwo));
+    // //output: 1,2,3,4,5,6
+    // const subscribe = concatSource.subscribe(val => console.log('Example 1: Basic concat:', val));
 
-    //delay 3 seconds then emit
-    const delayedSourceOne = sourceOne.pipe(delay(3000));
-    //sourceTwo waits on sourceOne to complete before subscribing
-    const concatDelayedSource = delayedSourceOne.pipe(concat(sourceTwo));
-    //output: 1,2,3,4,5,6
-    const subscribeDelayed = concatDelayedSource.subscribe(val => console.log('Example 2: Delayed source one:', val));
+    // //delay 3 seconds then emit
+    // const delayedSourceOne = sourceOne.pipe(delay(3000));
+    // //sourceTwo waits on sourceOne to complete before subscribing
+    // const concatDelayedSource = delayedSourceOne.pipe(concat(sourceTwo));
+    // //output: 1,2,3,4,5,6
+    // const subscribeDelayed = concatDelayedSource.subscribe(val => console.log('Example 2: Delayed source one:', val));
 
-    //when sourceOne never completes, the subsequent observables never run
-    const sourceOneNeverComplete =
-      pipe(concat(
-        interval(1000),
-        of('This', 'Never', 'Runs')
-      ))
-      //for logging clarity
-      .delay(5000)
-    //outputs: 1,2,3,4....
-    const subscribeNeverComplete = sourceOneNeverComplete.subscribe(val => console.log('Example 3: Source one never completes, second observable never runs:', val));
+    // //when sourceOne never completes, the subsequent observables never run
+    // const sourceOneNeverComplete =
+    //   pipe(concat(
+    //     interval(1000),
+    //     of('This', 'Never', 'Runs')
+    //   ))
+    //   //for logging clarity
+    //   .delay(5000)
+    // //outputs: 1,2,3,4....
+    // const subscribeNeverComplete = sourceOneNeverComplete.subscribe(val => console.log('Example 3: Source one never completes, second observable never runs:', val));
   }
 
 }
--- src/app/rxjs-concatall/rxjs-concatall.component.ts
+++ src/app/rxjs-concatall/rxjs-concatall.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, of, fromEvent } from 'rxjs';
+import { map, concatAll, take } from 'rxjs/operators';
+import { pipe } from '@angular/core/src/render3/pipe';
 
 @Component({
   selector: 'app-rxjs-concatall',
@@ -10,6 +13,35 @@ export class RxjsConcatallComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    //emit a value every 2 seconds
+    const sourceOne = interval(2000);
+    const example = sourceOne
+      //for demonstration, add 10 to and return as observable
+      .pipe(map(val => of(val + 10))
+        //merge values from inner observable
+        , concatAll());
+    //output: 'Example with Basic Observable 0', 'Example with Basic Observable 2'...
+    const subscribe = example.subscribe(val => console.log('Example with Basic Observable:', val));
+
+    //create and resolve basic promise
+    const samplePromise = val => new Promise(resolve => resolve(val));
+    const exampleTwo = sourceOne
+      .pipe(map(val => samplePromise(val))
+      //merge values from resolved promise
+      ,concatAll());
+    //output: 'Example with Promise 0', 'Example with Promise 1'...
+    const subscribeTwo = exampleTwo.subscribe(val => console.log('Example with Promise:', val));
+    this.onClick()
+  }
+
+  onClick(){
+    var clicks = fromEvent(document, 'click');
+    // var higherOrder = clicks.pipe(map((ev) => {
+    //   interval(1000),take(4)
+    // }));
+    // var firstOrder = higherOrder.pipe(concatAll());
+    // firstOrder.subscribe(x => console.log(x));
+
   }
 
 }
