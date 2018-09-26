53d68967fe42496bfa54abb1bf6ad4ebc0ddccc4
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-09-26 10:48:12 +0530

    skipWhile

--- src/app/rxjs-skip-while/rxjs-skip-while.component.ts
+++ src/app/rxjs-skip-while/rxjs-skip-while.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { skipWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs-skip-while',
@@ -10,6 +12,16 @@ export class RxjsSkipWhileComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.skipwhile();
+  }
+
+  skipwhile() {
+    //emit every 1s
+    const source = interval(1000);
+    //skip emitted values from source while value is less than 5
+    const example = source.pipe(skipWhile(val => val < 5));
+    //output: 5...6...7...8........
+    const subscribe = example.subscribe(val => console.log(val));
   }
 
 }
