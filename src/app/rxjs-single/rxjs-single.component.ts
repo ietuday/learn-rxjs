7cdded3f92f7fec1a2b8d2f04153931a0126bb67
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-09-26 10:41:15 +0530

    single

--- src/app/rxjs-single/rxjs-single.component.ts
+++ src/app/rxjs-single/rxjs-single.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { single } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs-single',
@@ -10,6 +12,16 @@ export class RxjsSingleComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.single();
+  }
+
+  single() {
+    //emit (1,2,3,4,5)
+    const source = from([1, 2, 3, 4, 5]);
+    //emit one item that matches predicate
+    const example = source.pipe(single(val => val === 4));
+    //output: 4
+    const subscribe = example.subscribe(val => console.log(val));
   }
 
 }
